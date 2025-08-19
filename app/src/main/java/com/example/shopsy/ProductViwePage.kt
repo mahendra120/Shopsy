b005e633399319efb4ee42ab9c2aa8713f36fcfc
gohilmahendra <gohilmahendra424@gmail.com>
2025-08-19 04:47:39 -0700

    all

--- app/src/main/java/com/example/shopsy/ProductViwePage.kt
+++ app/src/main/java/com/example/shopsy/ProductViwePage.kt
@@ -1,9 +1,12 @@
 package com.example.shopsy
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -14,11 +17,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import com.example.shopsy.Data.Products
 
 class ProductViwePage : ComponentActivity() {
+    lateinit var product: Products
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        product = intent.getSerializableExtra("product", Products::class.java) ?: Products()
         setContent {
             Scaffold(modifier = Modifier.Companion.fillMaxSize()) { innerPadding ->
                 Box(
@@ -36,6 +44,8 @@ class ProductViwePage : ComponentActivity() {
     @Preview(showSystemUi = true)
     @Composable
     fun Product() {
+        // jo ama khali show karvanu chhe dhyan rakhje
+        Log.d("============", "Product: $product")
 
     }
 }
