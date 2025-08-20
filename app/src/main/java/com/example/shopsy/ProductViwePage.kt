a0a5ee238962fde205e9fe5950f093a1ca77419f
gohilmahendra <gohilmahendra424@gmail.com>
2025-08-20 04:56:29 -0700

    all

--- app/src/main/java/com/example/shopsy/ProductViwePage.kt
+++ app/src/main/java/com/example/shopsy/ProductViwePage.kt
@@ -8,7 +8,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -21,9 +23,15 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowDownward
+import androidx.compose.material.icons.filled.ArrowUpward
+import androidx.compose.material.icons.filled.LocalShipping
+import androidx.compose.material.icons.filled.MailOutline
+import androidx.compose.material.icons.filled.ShoppingCartCheckout
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -32,6 +40,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -68,11 +80,11 @@ class ProductViwePage : ComponentActivity(), PaymentResultListener {
                     BottomAppBar(modifier = Modifier.fillMaxWidth()) {
                         Button(
                             onClick = {
-                                doPurchase("${product.price}")
+                                doPurchase(100)
                             },
                             modifier = Modifier
                                 .fillMaxSize()
-                                .padding(start = 15.dp, top = 15.dp),
+                                .padding(15.dp),
                             colors = ButtonDefaults.buttonColors(containerColor = Color(0, 0, 139))
                         )
                         {
@@ -102,7 +114,8 @@ class ProductViwePage : ComponentActivity(), PaymentResultListener {
     @Preview(showSystemUi = true)
     @Composable
     fun Product() {
-        // jo ama khali show karvanu chhe dhyan rakhje
+        var expanded by remember { mutableStateOf(false) }
+
         Log.d("============", "Product: $product")
         LazyColumn {
             item {
@@ -126,7 +139,7 @@ class ProductViwePage : ComponentActivity(), PaymentResultListener {
                 }
                 Text(
                     "Product : $name",
-                    fontSize = 18.sp,
+                    fontSize = 20.sp,
                     color = Color.Black,
                     fontFamily = font4,
                     fontWeight = FontWeight.Bold,
@@ -134,78 +147,138 @@ class ProductViwePage : ComponentActivity(), PaymentResultListener {
                     overflow = TextOverflow.Ellipsis,
                     modifier = Modifier.padding(top = 10.dp, start = 9.dp)
                 )
-                Spacer(modifier = Modifier.padding(top = 0.dp))
-                Text(
-                    "Brand : ${product.brand}",
-                    fontSize = 19.sp,
-                    fontFamily = font4,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.padding(top = 0.dp, start = 9.dp)
-                )
-                Row(modifier = Modifier.padding(top = 0.dp)) {
+                Spacer(modifier = Modifier.padding(top = 5.dp))
+                Row(modifier = Modifier) {
                     Text(
-                        "⭐️ ${product.rating} Very Good",
-                        fontSize = 17.sp,
-                        color = Color.Black,
+                        "Brand : ${product.brand} | ",
+                        fontSize = 18.sp,
                         fontFamily = font4,
                         fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(top = 5.dp, start = 9.dp)
+                        modifier = Modifier.padding(top = 0.dp, start = 9.dp)
                     )
-                    Spacer(modifier = Modifier.padding(top = 0.dp))
                     Text(
-                        "${product.availabilityStatus}",
-                        fontSize = 15.sp,
+                        "⭐️ ${product.rating} Rating",
+                        fontSize = 18.sp,
                         color = Color.Black,
                         fontFamily = font4,
                         fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(top = 6.dp, start = 3.dp)
+                        modifier = Modifier.padding(top = 0.dp, start = 2.dp)
                     )
                 }
-                Spacer(modifier = Modifier.padding(top = 0.dp))
-                Row(modifier = Modifier.padding(start = 10.dp)) {
+                Row(modifier = Modifier.padding(start = 10.dp, top = 5.dp)) {
                     Text(
-                        "Category : ${product.discountPercentage}%",
-                        fontSize = 20.sp,
+                        "Category : ${product.discountPercentage}% | ",
+                        fontSize = 18.sp,
                         color = Color(0, 100, 0),
                         fontFamily = font4,
                         fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(top = 5.dp, start = 0.dp)
+                        modifier = Modifier.padding(top = 0.dp, start = 0.dp)
                     )
-                    Spacer(Modifier.padding(7.dp))
+
                     Text(
                         "Price : $ ${product.price}",
-                        fontSize = 20.sp,
+                        fontSize = 18.sp,
                         color = Color.Black,
                         fontFamily = font4,
                         fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(top = 5.dp, start = 0.dp)
+                        modifier = Modifier.padding(top = 0.dp, start = 0.dp)
                     )
                 }
-                Spacer(modifier = Modifier.padding(top = 3.dp))
-                Text(
-                    "Quantity : ${product.minimumOrderQuantity}",
-                    fontSize = 21.sp,
-                    fontFamily = font4,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.padding(top = 0.dp, start = 9.dp)
-                )
-                Spacer(modifier = Modifier.padding(top = 3.dp))
-                Text(
-                    "Shipping : ${product.shippingInformation}",
-                    fontSize = 19.sp,
-                    fontFamily = font4,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.padding(start = 10.dp)
-                )
                 Spacer(modifier = Modifier.padding(top = 5.dp))
+                Row(modifier = Modifier.padding()) {
+                    Text(
+                        "Quantity : ${product.stock}",
+                        fontSize = 18.sp,
+                        fontFamily = font4,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(top = 0.dp, start = 9.dp)
+                    )
+                    Icon(
+                        Icons.Default.ShoppingCartCheckout,
+                        contentDescription = null,
+                        modifier = Modifier.padding(start = 7.dp, top = 3.dp),
+                        tint = Color.DarkGray.copy(.6f)
+                    )
+                    Text(
+                        "${product.availabilityStatus}",
+                        fontSize = 15.sp,
+                        color = Color(0, 0, 180),
+                        fontFamily = font4,
+                        modifier = Modifier.padding(top = 4.dp, start = 6.dp)
+                    )
+                }
+                Spacer(modifier = Modifier.padding(top = 5.dp))
+                Row(modifier = Modifier.padding()) {
+                    Text(
+                        "Shipping : ${product.shippingInformation}",
+                        fontSize = 18.sp,
+                        fontFamily = font4,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(start = 10.dp)
+                    )
+                    Icon(
+                        Icons.Default.LocalShipping,
+                        contentDescription = null,
+                        modifier = Modifier.padding(start = 7.dp, top = 3.dp),
+                        tint = Color.DarkGray.copy(.6f)
+                    )
+                }
                 Text(
-                    "Description : ${product.description}",
-                    fontSize = 19.sp,
+                    "Warranty : ${product.warrantyInformation}", fontSize = 18.sp,
+                    color = Color.Black,
                     fontFamily = font4,
                     fontWeight = FontWeight.Bold,
-                    modifier = Modifier.padding(top = 5.dp, start = 9.dp)
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier.padding(top = 2.dp, start = 9.dp)
                 )
-
+                Spacer(modifier = Modifier.padding(top = 15.dp))
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 10.dp)
+                        .clickable { expanded = !expanded }
+                ) {
+                    Column(modifier = Modifier.padding(16.dp)) {
+                        Row {
+                            Text(
+                                text = "Description",
+                                fontSize = 18.sp,
+                                fontFamily = font4,
+                                fontWeight = FontWeight.Bold,
+                                style = MaterialTheme.typography.bodyLarge
+                            )
+                            Spacer(modifier = Modifier.weight(1f))
+                            if (expanded) {
+                                Icon(
+                                    Icons.Default.ArrowUpward,
+                                    contentDescription = null,
+                                    modifier = Modifier
+                                        .size(20.dp)
+                                        .padding(top = 2.dp)
+                                )
+                            } else {
+                                Icon(
+                                    Icons.Default.ArrowDownward,
+                                    contentDescription = null,
+                                    modifier = Modifier
+                                        .size(20.dp)
+                                        .padding(top = 2.dp)
+                                )
+                            }
+                        }
+                        AnimatedVisibility(visible = expanded) {
+                            Text(
+                                "${product.description}",
+                                fontSize = 15.sp,
+                                fontFamily = font4,
+                                color = Color.DarkGray,
+                                fontWeight = FontWeight.Bold,
+                                modifier = Modifier.padding(top = 5.dp, start = 9.dp)
+                            )
+                        }
+                    }
+                }
             }
         }
     }
@@ -216,7 +289,7 @@ class ProductViwePage : ComponentActivity(), PaymentResultListener {
         TopAppBar(
             title = {
                 Text(
-                    "Brand : ${product.brand}",
+                    " ${product.brand}",
                     fontSize = 25.sp,
                     fontFamily = font4,
                     fontWeight = FontWeight.Bold,
@@ -235,24 +308,20 @@ class ProductViwePage : ComponentActivity(), PaymentResultListener {
         )
     }
 
-    private fun doPurchase(amount: String) {
-
+    private fun doPurchase(amount: Int) {
         val amount = Math.round(amount.toFloat() * 100)
-
         val checkout = Checkout()
-
         checkout.setKeyID("rzp_test_CKxIMtjjMGCpJo")
-
         val jsonObject = JSONObject()
         try {
             jsonObject.put("name", "First Payment")
             jsonObject.put("description", "Test payment compose Integration")
-            jsonObject.put("theme.color", "")
             jsonObject.put("currency", "INR")
             jsonObject.put("amount", amount)
             jsonObject.put("prefill.contact", "9106597990")
             jsonObject.put("prefill.email", "test@gmail.com")
 
+            Log.d("900904", "doPurchase: $amount")
             // open razorpay to checkout activity
             checkout.open(this@ProductViwePage, jsonObject)
         } catch (e: JSONException) {
